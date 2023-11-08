2a03da642e41b26a079dffda47e9082ec4110cd1
Robert Dixon <robmsc24@gmail.com>
2023-11-08 12:18:19 -0500

    Update changes

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,23 +1,69 @@
-import React from 'react'
-import axios from 'axios'
-import Character from './Character'
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
-const urlPlanets = 'http://localhost:9009/api/planets'
-const urlPeople = 'http://localhost:9009/api/people'
+const urlPeople = 'http://localhost:9009/api/people';
+const urlPlanets = 'http://localhost:9009/api/planets';
 
 function App() {
-  // ❗ Create state to hold the data from the API
-  // ❗ Create effects to fetch the data and put it in state
+  const [characters, setCharacters] = useState([]);
+  const [planets, setPlanets] = useState([]);
+  const [visiblePlanets, setVisiblePlanets] = useState(Array(characters.length).fill(false));
+
+  useEffect(() => {
+    // Fetch data from the people API
+    axios.get(urlPeople)
+      .then(response => {
+        // Extract character names from the API response and update the state
+        const characterNames = response.data;
+        setCharacters(characterNames);
+      })
+      .catch(error => {
+        console.error('Error fetching characters:', error);
+      });
+
+    // Fetch data from the planets API
+    axios.get(urlPlanets)
+      .then(response => {
+        // Extract planet names from the API response and update the state
+        const planetNames = response.data;
+        setPlanets(planetNames);
+      })
+      .catch(error => {
+        console.error('Error fetching planets:', error);
+      });
+  }, []); // Empty dependency array means this effect runs once after the initial render
+
+  const handleCharacterClick = (index) => {
+    // Toggle the visibility state for the clicked character's planet name
+    setVisiblePlanets(prevVisiblePlanets => {
+      const updatedVisiblePlanets = [...prevVisiblePlanets];
+      updatedVisiblePlanets[index] = !updatedVisiblePlanets[index];
+      return updatedVisiblePlanets;
+    });
+  };
+
   return (
     <div>
       <h2>Star Wars Characters</h2>
       <p>See the README of the project for instructions on completing this challenge</p>
-      {/* ❗ Map over the data in state, rendering a Character at each iteration */}
+      <div>
+        {characters.map((character, index) => (
+          <div key={index} className="character-card" onClick={() => handleCharacterClick(index)}>
+            <h3 className="character-name">{character.name}</h3>
+            {visiblePlanets[index] && (
+              <p className="planet-name">
+                Planet: <span className="character-planet">{planets.find(planet => planet.id == character.homeworld).name}</span>
+              </p>
+            )}
+          </div>
+        ))}
+      </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
+
 
 // ❗ DO NOT CHANGE THE CODE  BELOW
 if (typeof module !== 'undefined' && module.exports) module.exports = App
--- frontend/components/Character.js
+++ frontend/components/Character.js
@@ -1,13 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react';
 
-function Character() { // ❗ Add the props
-  // ❗ Create a state to hold whether the homeworld is rendering or not
-  // ❗ Create a "toggle" click handler to show or remove the homeworld
+function Character(props) {
+  const [isHomeworldVisible, setIsHomeworldVisible] = useState(false);
+
+  const toggleHomeworld = () => {
+    setIsHomeworldVisible(prevState => !prevState);
+  };
+
   return (
-    <div>
-      {/* Use the same markup with the same attributes as in the mock */}
+    <div className="character-card" onClick={toggleHomeworld}>
+      <h3 className="character-name">{props.name}</h3>
+      {isHomeworldVisible && (
+        <p className="planet-name">Planet: {props.homeworld}</p>
+      )}
     </div>
-  )
+  );
 }
 
-export default Character
+export default Character;
